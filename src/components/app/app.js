c5c5a9898afc0d9e5b6dc9e4c606384f37c16f7a
Mathias <mathiasmichaelweiss@gmail.com>
2020-12-09 18:04:21 +0100

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+import './app.css';
+import '../app-header/app-header.css';
+import '../post-add-form/post-add-form.css';
+import '../post-list/post-list.css';
+import '../post-list-item/post-list-item.css';
+import '../post-status-filter/post-status-filter.css';
+import '../search-panel/search-panel';
+
+const App = () => {
+
+    const data = [
+        { label: 'Goin to learn React', important: true, id: 'qwe' },
+        { label: 'That is good', important: false, id: 'wer' },
+        { label: 'I need a break...', important: false, id: 'sdf' }
+    ];
+
+    return (
+        <div className="app">
+            <AppHeader />
+            <div className="search-panel d-flex">
+                <SearchPanel />
+                <PostStatusFilter />
+            </div>
+            <PostList posts={data} />
+            <PostAddForm />
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/app/';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Класс
+/* class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 26,
+            love: "Veronica",
+            mweis: "Mathias",
+            nationality: 'uk'
+        } */
+/* this.nextYear = this.nextYear.bind(this); */
+/*         this.nextYear = () => {
+            this.setState(state => ({
+                years: ++state.years
+            }))
+        }
+    } */
+/*  nextYear() {
+     console.log(1);
+     this.setState(state => ({
+         years: ++state.years
+     }))
+ } */
+/*     render() {
+        const { name, surname, link } = this.props;
+        const { years } = this.state;
+        const { love } = this.state;
+        const { mweis } = this.state;
+        return (
+            <>
+                <button onClick={this.nextYear}>++</button>
+                <h1>My name is {name}, surname - {surname}, years - {years}</h1>
+                <a href={link}>{love} + {mweis}</a>
+            </>
+        )
+    }
+} */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Компонент
+/* function WhoAmI({ name, surname, link }) {
+    return (
+        <React.Fragment>
+            <h1>My name is {name}, surname - {surname}</h1>
+            <a href={link}>Me profile</a>
+        </React.Fragment>
+    )
+} */
+
+/* const All = () => {
+    return (
+        <>
+            <WhoAmI name="John" surname="Smith" link="facebook.com" />
+            <WhoAmI name="Ivan" surname="Smith" link="facebook.com" />
+            <WhoAmI name="Ashot" surname="Smith" link="facebook.com" />
+        </>
+    )
+} */
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 reportWebVitals();
